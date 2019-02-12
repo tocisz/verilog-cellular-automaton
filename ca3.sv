4ad85279a7da7a15d5c1d53eb1c24f271c3f0df8
Tomasz Cichocki <tcichocki@axit.pl>
2019-02-12 19:34:07 +0100

    parameterize WIDTH

--- ca3.sv
+++ ca3.sv
@@ -5,15 +5,17 @@ module ca3(
   input wire       left,
   input wire       right,
 
-  input wire [31:0] state,
+  input wire [WIDTH-1 : 0] state,
   input wire       set_state,
 
-  output wire [31:0] out
+  output wire [WIDTH-1 : 0] out
 );
+
+  parameter WIDTH = 32;
 
   genvar i;
   generate
-  for (i=0; i <= 31; i=i+1)
+  for (i=0; i < WIDTH; ++i)
     ca_cell ith(
       .clk,
       .rule,
@@ -22,7 +24,7 @@ module ca3(
       .out(out[i]),
 
       .left(i == 0 ? left : out[i-1]),
-      .right(i == 31 ? right : out[i+1])
+      .right(i == WIDTH-1 ? right : out[i+1])
     );
   endgenerate
 
